2d9fc3741eb9cbce8925124164e4f98d7e7991cb
tanimsk <sktanim5800@gmail.com>
2023-02-06 00:34:42 +0600

    init

--- interact.py
+++ interact.py
@@ -8,11 +8,11 @@ class Interact:
         self.chatbot = ChatBot('Diana Burnwood')
 
         # Create a new trainer for the chatbot
-        trainer = ChatterBotCorpusTrainer(self.chatbot)
+        # trainer = ChatterBotCorpusTrainer(self.chatbot)
 
-        # Now let us train our bot with multiple corpus
-        trainer.train("chatterbot.corpus.english.greetings",
-                    "chatterbot.corpus.english.conversations")
+        # # Now let us train our bot with multiple corpus
+        # trainer.train("chatterbot.corpus.english.greetings",
+        #             "chatterbot.corpus.english.conversations")
 
     def talk(self, msg) -> str:
         return self.chatbot.get_response(msg)
